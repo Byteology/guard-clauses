8f3a563a9ab7c05c4235afd736587ad070323540
ts.Igov <ts.igov@outlook.com>
2023-01-07 02:07:42 +0200

    Implemented the option that allows the regex guard to timeout.

--- Byteology.GuardClauses/Extensions/StringExtensions.cs
+++ Byteology.GuardClauses/Extensions/StringExtensions.cs
@@ -42,18 +42,53 @@ public static class StringExtensions
 	/// </summary>
 	/// <param name="clause">The guard clause containing the argument to guard.</param>
 	/// <param name="regexPattern">The regular expression pattern to match.</param>
+	/// <exception cref="ArgumentException">The argument is not in the format specified by a regular expression.</exception>
+	/// <exception cref="ArgumentNullException">The argument or provided pattern are <see langword="null"/>.</exception>
+	public static IGuardClause<string> InFormat(
+		this IGuardClause<string> clause,
+		string regexPattern)
+	{
+		return InFormat(clause, regexPattern, RegexOptions.None);
+	}
+
+	/// <summary>
+	/// Throws an <see cref="ArgumentException"/> if the argument is not in the format specified by a regular expression.
+	/// Throws an <see cref="ArgumentNullException"/> if the argument or the pattern are <see langword="null"/>.
+	/// </summary>
+	/// <param name="clause">The guard clause containing the argument to guard.</param>
+	/// <param name="regexPattern">The regular expression pattern to match.</param>
 	/// <param name="options">A bitwise combination of the enumeration values that provide options for matching.</param>
 	/// <exception cref="ArgumentException">The argument is not in the format specified by a regular expression.</exception>
 	/// <exception cref="ArgumentNullException">The argument or provided pattern are <see langword="null"/>.</exception>
 	public static IGuardClause<string> InFormat(
 		this IGuardClause<string> clause,
 		string regexPattern,
-		RegexOptions options = RegexOptions.None)
+		RegexOptions options)
+	{
+		return InFormat(clause, regexPattern, RegexOptions.None, Regex.InfiniteMatchTimeout);
+	}
+
+	/// <summary>
+	/// Throws an <see cref="ArgumentException"/> if the argument is not in the format specified by a regular expression.
+	/// Throws an <see cref="ArgumentNullException"/> if the argument or the pattern are <see langword="null"/>.
+	/// </summary>
+	/// <param name="clause">The guard clause containing the argument to guard.</param>
+	/// <param name="regexPattern">The regular expression pattern to match.</param>
+	/// <param name="options">A bitwise combination of the enumeration values that provide options for matching.</param>
+	/// <param name="matchTimeout">A time-out interval, or <see cref="System.Text.RegularExpressions.Regex.InfiniteMatchTimeout" /> to indicate that the method should not time out.</param>
+	/// <exception cref="ArgumentException">The argument is not in the format specified by a regular expression.</exception>
+	/// <exception cref="ArgumentNullException">The argument or provided pattern are <see langword="null"/>.</exception>
+	/// <exception cref="T:System.ArgumentOutOfRangeException">Thrown when no match is found in the specified time interval</exception>
+	public static IGuardClause<string> InFormat(
+		this IGuardClause<string> clause,
+		string regexPattern,
+		RegexOptions options,
+		TimeSpan matchTimeout)
 	{
 		Guard.Argument(clause.Argument, clause.ArgumentName).NotNull();
 		Guard.Argument(regexPattern, nameof(regexPattern)).NotNull();
 
-		Match match = Regex.Match(clause.Argument!, regexPattern, options);
+		Match match = Regex.Match(clause.Argument!, regexPattern, options, matchTimeout);
 		if (!match.Success || clause.Argument != match.Value)
 			throw new ArgumentException($"{clause.ArgumentName} should be in {regexPattern} format.");
 
